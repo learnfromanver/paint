cd6efa3192135ceb497c2b6fc636c03682d041fc
learnfromanver <63973978+learnfromanver@users.noreply.github.com>
2020-06-08 12:17:38 +0530

    First Commit

---
+++ Paint Form/PaintForm/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PaintForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ColorPickerButton = new System.Windows.Forms.Button();
+            this.PenButton = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.FillColorCheckBox = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(711, 501);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ColorPickerButton
+            // 
+            this.ColorPickerButton.Location = new System.Drawing.Point(729, 12);
+            this.ColorPickerButton.Name = "ColorPickerButton";
+            this.ColorPickerButton.Size = new System.Drawing.Size(92, 90);
+            this.ColorPickerButton.TabIndex = 1;
+            this.ColorPickerButton.Text = "Color Picker";
+            this.ColorPickerButton.UseVisualStyleBackColor = true;
+            this.ColorPickerButton.Click += new System.EventHandler(this.ColorPickerButton_Click);
+            // 
+            // PenButton
+            // 
+            this.PenButton.Location = new System.Drawing.Point(729, 169);
+            this.PenButton.Name = "PenButton";
+            this.PenButton.Size = new System.Drawing.Size(92, 45);
+            this.PenButton.TabIndex = 2;
+            this.PenButton.Text = "Pen";
+            this.PenButton.UseVisualStyleBackColor = true;
+            this.PenButton.Click += new System.EventHandler(this.PenButtond_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(729, 108);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(92, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(865, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // FillColorCheckBox
+            // 
+            this.FillColorCheckBox.AutoSize = true;
+            this.FillColorCheckBox.Location = new System.Drawing.Point(729, 343);
+            this.FillColorCheckBox.Name = "FillColorCheckBox";
+            this.FillColorCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.FillColorCheckBox.TabIndex = 4;
+            this.FillColorCheckBox.Text = "Fill Color";
+            this.FillColorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(729, 366);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 45);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Rectangle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.PenButtond_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(729, 277);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 45);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Line";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.PenButtond_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(729, 417);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(92, 45);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Circle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.PenButtond_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(729, 468);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(92, 45);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Triangle";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.PenButtond_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 527);
+            this.Controls.Add(this.FillColorCheckBox);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.PenButton);
+            this.Controls.Add(this.ColorPickerButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ColorPickerButton;
+        private System.Windows.Forms.Button PenButton;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox FillColorCheckBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Paint Form/PaintForm/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PaintForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadPictureBox();
+
+            // Make the pen selected by default
+            selectedShapeButton = PenButton;
+            selectedShapeButton.BackColor = Color.Red; 
+        }
+
+        Bitmap paintImage;
+        Graphics paintGraphics;
+
+        private void loadPictureBox()
+        {
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+
+            paintImage = new Bitmap(width, height);
+
+            paintGraphics = Graphics.FromImage(paintImage);
+
+            paintGraphics.FillRectangle(Brushes.White, 0, 0, width, height);
+            
+            pictureBox1.Image = paintImage;
+
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+        }
+
+        Point lastPoint;
+        bool isMouseDown = false;
+
+        void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+        }
+                
+        void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                if (selectedShapeButton.Text == "Pen")
+                {
+                    DrawLineInCanvas(e.Location);
+                }
+                else
+                {
+                    DrawShapeInWorkingImage(e.Location);
+                }
+            }
+        }
+
+        void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+
+            if (selectedShapeButton.Text != "Pen")
+            {
+                // Draw again to get the shape without border
+                DrawShapeInWorkingImage(e.Location);
+
+                paintImage = new Bitmap(workingImage);
+
+                // Re initialize graphics object as the bitmap is new
+                paintGraphics = Graphics.FromImage(paintImage); 
+
+                pictureBox1.Image = paintImage;                
+            }
+        }
+
+        private void DrawLineInCanvas(Point currentPoint)
+        {
+            // Form pen with the color selected and the size value in tracker
+            Pen pen = new Pen(colorPicker.Color, trackBar1.Value);
+
+            paintGraphics.DrawLine(pen, lastPoint, currentPoint);
+
+            lastPoint = currentPoint;
+
+            pictureBox1.Refresh();
+        }
+        
+        ColorDialog colorPicker = new ColorDialog();
+
+        private void ColorPickerButton_Click(object sender, EventArgs e)
+        {
+            colorPicker.ShowDialog();
+        }
+        
+        Bitmap workingImage;
+        Graphics workingGraphics;
+
+        private void DrawShapeInWorkingImage(Point currentPoint)
+        {
+            Pen pen = new Pen(colorPicker.Color, trackBar1.Value);
+
+            workingImage = new Bitmap(paintImage);
+            workingGraphics = Graphics.FromImage(workingImage);
+
+            int startPointX = lastPoint.X < currentPoint.X ? lastPoint.X : currentPoint.X;
+            int startPointY = lastPoint.Y < currentPoint.Y ? lastPoint.Y : currentPoint.Y;
+
+            int shapeWidth = (lastPoint.X > currentPoint.X ? lastPoint.X : currentPoint.X) - startPointX;
+            int shapeHeight = (lastPoint.Y > currentPoint.Y ? lastPoint.Y : currentPoint.Y) - startPointY;
+
+            switch (selectedShapeButton.Text)
+            {
+                case "Rectangle":
+                    // Check if it is to draw or fill shape
+                    if (!FillColorCheckBox.Checked)
+                    {
+                        // Draw Rectangle
+                        workingGraphics.DrawRectangle(pen, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    else
+                    {
+                        // Fill Rectangle
+                        workingGraphics.FillRectangle(pen.Brush, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    break;
+
+                case "Circle":
+                    if (!FillColorCheckBox.Checked)
+                    {
+                        workingGraphics.DrawEllipse(pen, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    else
+                    {
+                        workingGraphics.FillEllipse(pen.Brush, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    break;
+
+                case "Triangle":
+                    Point point1 = new Point() { X = startPointX, Y = startPointY + shapeHeight };
+                    Point point2 = new Point() { X = startPointX + (shapeWidth / 2), Y = startPointY };
+                    Point point3 = new Point() { X = startPointX + shapeWidth, Y = startPointY + shapeHeight };
+
+                    if (!FillColorCheckBox.Checked)
+                    {
+                        workingGraphics.DrawPolygon(pen, new Point[] { point1, point2, point3 });
+                    }
+                    else
+                    {
+                        workingGraphics.FillPolygon(pen.Brush, new Point[] { point1, point2, point3 });
+                    }
+                    break;
+
+                case "Line":
+                    workingGraphics.DrawLine(pen, lastPoint, currentPoint);
+                    break;
+            }
+
+            // The outline should be shown only if it is not a line and the drawing is on. 
+            if (isMouseDown && selectedShapeButton.Text != "Line") 
+            {
+                // Draw outline while drawing shapes
+                Pen outLinePen = new Pen(Color.Black);
+                outLinePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+
+                workingGraphics.DrawRectangle(outLinePen, startPointX, startPointY, shapeWidth, shapeHeight);
+            }
+
+            pictureBox1.Image = workingImage;
+        }
+        
+
+        Button selectedShapeButton;
+
+        private void PenButtond_Click(object sender, EventArgs e)
+        {
+            selectedShapeButton.BackColor = SystemColors.Control;
+
+            Button clickedButton = sender as Button;
+            clickedButton.BackColor = Color.Red;
+
+            selectedShapeButton = clickedButton;
+        }
+    }
+}
